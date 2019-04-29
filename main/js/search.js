440f1898b55b42cd27955bcba18534f238ff5919
limeng <1220224890@qq.com>
2019-04-29 13:27:43 +0800

    搜索模糊匹配

--- main/js/search.js
+++ main/js/search.js
@@ -1,10 +1,13 @@
 $(function () {
     $('head').append("<link rel='stylesheet' href='../../css/search.css'>");
-    var $searchLabel = $(".search-label input");
-    $searchLabel.focus(function(){
-        $searchLabel.parent().addClass("active-label");
-    });
-    $searchLabel.blur(function(){
-        $searchLabel.parent().removeClass("active-label");
-    })
+    //
+    //
+    //
+    // var $searchLabel = $(".search-label input");
+    // $searchLabel.focus(function(){
+    //     $searchLabel.parent().addClass("active-label");
+    // });
+    // $searchLabel.blur(function(){
+    //     $searchLabel.parent().removeClass("active-label");
+    // })
 });
