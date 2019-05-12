08ec536865d99d725cfc9b2f6a06f04165b6c286
limeng <1220224890@qq.com>
2019-05-12 16:52:08 +0800

    添加图片

--- main/config/navigatorPhotos.config.js
+++ main/config/navigatorPhotos.config.js
@@ -15,10 +15,20 @@ $(function () {
                  "src": "__IMG__/01.jpg" //地址
              }*/
             navigatorPhotos:[
-                {"alt": "图片介绍1", "title": "太湖西山~苏州", "src": encodeURI("/images/navigator/太湖西山1.jpg")},
-                {"alt": "图片介绍2", "title": "太湖西山~苏州", "src": encodeURI("/images/navigator/太湖西山2.png")},
+                {"title": "太湖西山~苏州", "src": encodeURI("/images/navigator/太湖西山1.jpg") ,"alt": "图片介绍1"},
                 {"alt": "图片介绍3", "title": "太湖西山~苏州", "src": encodeURI("/images/navigator/太湖西山3.png")},
-                {"alt": "图片介绍3", "title": "太湖~鼋头渚", "src": encodeURI("/images/navigator/鼋头渚-无锡.png")}
+                {"alt": "图片介绍3", "title": "太湖鼋头渚~无锡", "src": encodeURI("/images/navigator/鼋头渚-无锡.jpg")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦1.jpg")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦2.jpg")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦3.jpg")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦4.jpg")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦5.jpg")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦6.jpg")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦7.jpg")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦8.jpg")},
+                {"alt": "图片介绍2", "title": "太湖西山~苏州", "src": encodeURI("/images/navigator/太湖西山2.png")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦9.jpg")},
+                {"alt": "图片介绍3", "title": "东冲半岛~霞浦", "src": encodeURI("/images/navigator/福建霞浦10.jpg")}
             ],
             blog:[]
         };
